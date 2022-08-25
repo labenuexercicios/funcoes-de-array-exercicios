86c90bfc49dc53fc19ef8203945a05b692d37258
Nhaykwollfer <108698094+Nhaykwollfer@users.noreply.github.com>
2022-08-25 20:37:04 -0300

    Gostaria de agradecer a Prof Juliana Moraes, essa maravilhosa pela paci~encia e ecplicação do exercício, e ao Aluno Carlos Nogueira pela logica e ajuda nos créditos do exercicío 1

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,3 +1,7 @@
+//  # Exercício 3
+
+// Dado um array de produtos, onde cada produto é um objeto com nome, preço e categoria, retorne um novo array com o nome dos produtos da categoria Limpeza.
+
 const produtos = [
     { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
     { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
@@ -10,4 +14,9 @@ const produtos = [
     { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
     { nome: "Sabão em Pó", categoria: "Limpeza", preco: 10.80 }
  ]
- 
+
+ const prodLimpe = produtos.filter((limpeza) =>{
+    return limpeza.categoria === "Limpeza"
+ })
+
+ console.log(`Produtos de limpeza:`, prodLimpe)
