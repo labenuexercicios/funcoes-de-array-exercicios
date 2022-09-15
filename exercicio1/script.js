f37bc22de8bdf26b61e1292d39e3c9fe2223c991
Raissa Baeta <rbaetadev@gmail.com>
2022-09-15 15:55:12 -0300

    Exercícios 1,2,3

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,3 +4,32 @@ const objeto = {
     username: "astrodev_labenu",
     senha: "melhorDeTodos"
 }
+
+console.log(objeto.nome)
+
+const caixaAlta = (meuObjeto) => {
+    meuObjeto.nome = meuObjeto.nome.toUpperCase();
+    meuObjeto.profissao = meuObjeto.profissao.toUpperCase();
+    meuObjeto.username = meuObjeto.username.toUpperCase();
+    meuObjeto.senha = meuObjeto.senha.toUpperCase();
+    
+    console.log(meuObjeto)
+    
+}
+
+const textoCorrido = (meuObjeto) =>{
+    let texto = null;
+    texto = meuObjeto.nome;
+    texto += meuObjeto.profissao = meuObjeto.profissao;
+    texto += meuObjeto.username = meuObjeto.username;
+    texto += meuObjeto.senha = meuObjeto.senha
+    console.log(texto)
+}
+
+const join = (meuObjeto, funcaoCaixaAlta, funcaoTextoCorrido) => {
+    funcaoCaixaAlta(meuObjeto);
+    funcaoTextoCorrido(meuObjeto)
+}
+
+join(objeto,caixaAlta,textoCorrido)
+
