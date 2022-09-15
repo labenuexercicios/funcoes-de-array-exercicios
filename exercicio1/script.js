4ca74ca52d833be12263a0550da2ccf4bab8156e
italodve <italovinicius@Gmail.com>
2022-09-15 16:27:28 -0300

    feito

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -3,4 +3,32 @@ const objeto = {
     profissao: "Dev das estrelas",
     username: "astrodev_labenu",
     senha: "melhorDeTodos"
+} 
+
+
+
+const transformaObjeto = (array) => {
+return{
+    nome:array.nome.toUpperCase(),
+    profissao:array.profissao.toUpperCase(),
+    username:array.username.toUpperCase(),
+    senha:array.senha.toUpperCase(),
 }
+}
+console.log(transformaObjeto(objeto)) 
+
+const transformaObjeto2 = (array) => {
+    let texto = `${array.nome} ${array.profissao} ${array.username } ${array.senha}`
+return texto
+
+} 
+console.log(transformaObjeto2(objeto)) 
+
+
+
+const CBack = (objeto, callback) => {
+    console.log(callback(objeto))
+}
+
+CBack(objeto, transformaObjeto)
+CBack(objeto, transformaObjeto2)
---
+++ exercicio2/script.js
@@ -0,0 +1,7 @@
+const number = [15,20,30,50,60,70,80,90]
+
+
+const Nx3 = number.map((N) =>{
+    return N*3
+})
+console.log(Nx3)  
