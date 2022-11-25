281dd20c8bb7887b40e34c3b956da247a2c7194b
Renan <ren4n_oliveira@hotmail.com>
2022-11-24 21:06:03 -0300

    Exercicios feitos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,3 +4,97 @@ const objeto = {
     username: "astrodev_labenu",
     senha: "melhorDeTodos"
 }
+
+
+const objetoFuncao1 = function(alterarValores){
+    
+
+
+     return alterarValores = {nome:alterarValores.nome.toUpperCase(),
+    profissao: alterarValores.profissao.toUpperCase(),username: alterarValores.username.toUpperCase(),
+    senha: alterarValores.senha.toUpperCase()}
+                 
+}
+ //console.log(objetoFuncao1(objeto))
+
+
+
+
+const textoCorrido = function (objeto) { return`Olá ${objeto.nome} ${objeto.profissao}, ${objeto.username}, ${objeto.senha}`
+}
+
+//console.log(textoCorrido(objeto))
+
+
+
+const novaFuncao = function(alterarValores, callback){
+    console.log(callback(alterarValores))
+}
+
+
+novaFuncao(objeto, objetoFuncao1)
+novaFuncao(objeto, textoCorrido)
+
+
+
+
+//EXTRA
+
+/* const mostraIdade = (idade)=>{
+  console.log("Sua idade é " ,idade)
+}
+
+const calcularIdade = (anoAtual,anoNasc,callback)=>{
+  
+ const calculoAno = anoAtual - anoNasc
+ callback(calculoAno)
+
+}
+
+calcularIdade (2022,1994,mostraIdade) */
+
+/* const mostrarJogador = (jogador)=>{
+  console.log("O melhor jogador é: ", jogador)
+  }
+
+  const nomeJogador = (nome,callback)=>{
+    const resultado = nome
+    callback(resultado)
+  }
+
+  
+  nomeJogador ("Gabriel Jesus",mostrarJogador)
+
+
+ */
+
+/* 
+  const mostrarCantor = (cantor) =>{
+    console.log("O cantor é " +cantor)
+  }
+
+  const nomeCantor = (callback)=>{
+    let nome = prompt("Digite o nome do cantor")
+    callback(nome)
+  }
+
+  nomeCantor (mostrarCantor)  */
+
+  const verificaAtividade =(hora) =>{
+    if(hora === 7) {
+      return "trabalhar"
+    } else if (hora === 12){
+      return "almoçar"
+    }else if (hora ===23){
+      return "dormir"
+    }else return "estudar"
+  }
+
+  const imprimeAtividade = (horaAtual,callback)=>{
+    const atividade = callback(horaAtual)
+    return `agora é hora de ${atividade}`
+  }
+
+  console.log(imprimeAtividade(7, verificaAtividade))
+
+
---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+
+
+const arrayNumeros= [2,8,15,26,27,60, 63,78]
+
+const triplo = arrayNumeros.map((CadaElemento)=> CadaElemento * 3)
+
+const dividido = arrayNumeros.map((Elementos) => Elementos/2)
+
+
+console.log(triplo)
+console.log(dividido)
